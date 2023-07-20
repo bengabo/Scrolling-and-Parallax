a27078c12cadd8034801823e79ae66645232ed58
bengabo <ben@bengabo.com>
2023-07-20 10:05:13 +0200

    header cue no longer appears while scrolling down

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,24 @@
 let meetTheMonsters = document.querySelector("#meet");
 let navbar = document.querySelector(".site-nav");
-let sticky = navbar.offsetTop;
 let navHeight = navbar.scrollHeight;
+let headerContent = document.querySelector(".header-content");
+let headerCue = document.querySelector(".header-cue");
+let headerCueHeight = headerCue.scrollHeight;
 
 const moveHeader = () => {
   let mainOnTop = meetTheMonsters.getBoundingClientRect().top - navHeight;
+
   mainOnTop < 0
     ? navbar.classList.add("in-body")
     : navbar.classList.remove("in-body");
+
+  let currentCuePosition = headerContent.getBoundingClientRect().top;
+  console.log(currentCuePosition);
+  currentCuePosition < -300
+    ? headerCue.classList.add("d-none")
+    : headerCue.classList.remove("d-none");
+
   window.requestAnimationFrame(moveHeader);
 };
 
 window.requestAnimationFrame(moveHeader);
-
-// window.addEventListener("scroll", function () {
-//   // Check if the user has scrolled past the navbar
-//   if (window.scrollY >= sticky) {
-//     navbar.classList.add("in-body");
-//   } else {
-//     navbar.classList.remove("in-body");
-//   }
-// });
-
-// const moveHeader = () => {
-//   let mainOnTop = meetTheMonsters.getBoundingClientRect().top - navHeight;
-//   console.log(mainOnTop);
-//   mainOnTop < 0
-//     ? nav.classList.add("in-body")
-//     : nav.classList.remove("in-body");
-//   window.requestAnimationFrame(moveHeader);
-// };
-
-// window.requestAnimationFrame(moveHeader);
